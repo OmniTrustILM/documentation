9e622b025e00919507f2271f949ae120a55d3d90
3keyroman <46850604+3keyroman@users.noreply.github.com>
2025-03-21 07:57:18 +0100

    Updates for release 2.14.1 (#191)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,8 @@
 const lightCodeTheme = require('prism-react-renderer').themes.github;
 const darkCodeTheme = require('prism-react-renderer').themes.dracula;
 
-const apiVersion = '2.14.0';
-const chartVersion = '2.14.0';
+const apiVersion = '2.14.1-fix';
+const chartVersion = '2.14.1';
 
 import remarkFindReplacePlugin from './src/plugins/remarkFindReplacePlugin.mjs';
 import remarkSimplePlantumlPlugin from './src/plugins/remarkSimplePlantumlPlugin.mjs';
