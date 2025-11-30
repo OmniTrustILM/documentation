89a9f9bec6367d223560300c0a17b507690df2e1
3keyroman <46850604+3keyroman@users.noreply.github.com>
2025-11-30 14:30:16 +0100

    Updates for CSC component version 1.4.0

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,7 +3,7 @@ const darkCodeTheme = require('prism-react-renderer').themes.dracula;
 
 const apiVersion = '2.16.1';
 const chartVersion = '2.16.1';
-const cscVersion = '1.3.0';
+const cscVersion = '1.4.0';
 
 import remarkFindReplacePlugin from './src/plugins/remarkFindReplacePlugin.mjs';
 import remarkSimplePlantumlPlugin from './src/plugins/remarkSimplePlantumlPlugin.mjs';
